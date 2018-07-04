45a31013a65b0cb2632aa977b0b9b07e3315d113
harryphelps2 <harryphelps1@gmail.com>
2018-07-04 11:15:26 +0100

    Add determine high score function

--- run.py
+++ run.py
@@ -1,2 +1,15 @@
-def is_correct():
-    return False
+def is_correct(guess, answer):
+    if (answer == guess):
+        return True
+
+def increment_score(old_score):
+    new_score = old_score =+ 1
+    return new_score
+
+def determine_high_score(high_score, new_score):
+    if high_score < new_score:
+        high_score = new_score
+    return high_score
+
+if __name__ == '__main__':
+    main()
