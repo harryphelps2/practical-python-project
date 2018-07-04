8afb0d8e1dea0c52b3648de8476d9f4d272644da
harryphelps2 <harryphelps1@gmail.com>
2018-07-04 11:15:50 +0100

    Add determine high score test

--- test_run.py
+++ test_run.py
@@ -7,8 +7,21 @@ class TestRiddle(unittest.TestCase):
     """
     def test_is_correct(self):
         """Test to see if the guessed answer is correct or not"""
-        answer = run.is_correct()
-        self.assertTrue(answer)
+        correct = run.is_correct(3,3)
+        self.assertTrue(correct)
+    
+    def test_increment_score(self):
+        """Test to see if the score is incremented by 1
+        if the answer is correct"""
+        new_score = run.increment_score(0)
+        self.assertEqual(new_score, 1)
+
+    def test_determine_high_score(self):
+        """Test to see if the new score is a high score"""
+        high_score = 1
+        new_score = 2
+        high_score = run.determine_high_score(high_score, new_score)
+        self.assertEqual(high_score, new_score)
 
 if __name__ == '__main__':
     unittest.main()
