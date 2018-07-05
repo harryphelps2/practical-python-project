e1d4b39b95b7e82363a3cc97eb2ffc0aef4c1a54
harryphelps2 <harryphelps1@gmail.com>
2018-07-05 17:12:20 +0100

    Fix bug that wasn't incrementing the score

--- run.py
+++ run.py
@@ -14,21 +14,16 @@ def is_correct(guess, answer):
     if (answer == guess):
         return True
 
-def increment_score(old_score):
-    new_score = old_score =+ 1
+def increment_score(username, score):
+    new_score = score + 1
+    update_score(username, new_score)
     return new_score
 
-def determine_high_score(high_score, new_score):
-    if high_score < new_score:
-        high_score = new_score
-    return high_score
-
-def submit_guess(score, guess, answer):
-    if request.method == 'POST':
-        if is_correct(guess, answer):
-            increment_score(score)
-        else:
-            print("incorrect")
+def submit_guess(username, score, guess, answer):
+    if is_correct(guess, answer):
+        increment_score(username, score)
+    else:
+        print("incorrect")
     return score
 
 def add_user(username):
@@ -37,8 +32,17 @@ def add_user(username):
         users[username] = 0
     with open("data/users.txt", "w") as f:
         f.write(json.dumps(users, sort_keys=True, indent=4, separators=(',', ': ')))
-    
-    
+
+def update_score(username, score):
+    with open("data/users.txt", "r+") as f:
+        users = json.loads(f.read())
+        users[username] = score
+    #maybe this is happening outside the other functiom? let's put them into one with r+ or w+
+    #maybe reading from the cursor so can't update
+    with open("data/users.txt", "w+") as f:
+        f.write(json.dumps(users, sort_keys=True, indent=4, separators=(',', ': ')))
+    return score
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """Main page with instructions for playing redirects to username url"""
@@ -52,9 +56,15 @@ def index():
 def playgame(username):
     with open("data/users.txt", "r") as f:
         users = json.loads(f.read())
-    score = users[username]
+        score = users[username]
+    print(score)
     with open("data/riddles.json", "r") as riddles_data:
         riddles = json.load(riddles_data)
+    if request.method == "POST":
+        #when we set the high score to 2 it goes back to 1
+        #think maybe the score is setting back to 0 and incerasing by 1 each time
+        submit_guess(username, score, request.form["guess"], "5")
+        return redirect(username)
     return render_template("riddles.html", riddles=riddles, score=score)
 
 if __name__ == '__main__':
